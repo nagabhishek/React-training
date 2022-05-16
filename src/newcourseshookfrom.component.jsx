9e602af966dac4c80fcf4ec80bf44ba2540c4997
nagabhishek <nag.abhishek2012@gmail.com>
2022-05-16 19:06:11 +0530

    1 file modified

--- src/newcourseshookfrom.component.jsx
+++ src/newcourseshookfrom.component.jsx
@@ -11,13 +11,25 @@ export default function NewCourseWithReactHookForms() {
     handleSubmit,
     formState: { errors },
   } = useForm({ mode: 'onChange' });
+
   return (
     <div className="row justify-content-md-center m-4">
       <form
         className="col-md-4 alert alert-secondary"
         onSubmit={() => {
-          handleSubmit((data) => console.log(data));
-          navigate('/');
+          handleSubmit((data) => {
+            console.log(data);
+            const obj = {
+              id: data.CourseId,
+              title: data.CourseName,
+              price: data.CoursePrice,
+              likes: data.CourseLikes,
+              rating: data.CourseRating,
+              imageUrl: data.CourseImage,
+            };
+            coursesCtx.addCourse(obj);
+            navigate('/');
+          });
         }}
       >
         <h2>New Course</h2>
@@ -49,13 +61,53 @@ export default function NewCourseWithReactHookForms() {
           <p style={{ color: 'red' }}>{errors.CourseName.message}</p>
         )}
         <label htmlFor="txtCoursePrice">Price : </label>
-        <input type="number" id="txtCoursePrice" className="form-control" />
+        <input
+          type="number"
+          id="txtCoursePrice"
+          className="form-control"
+          {...register('CoursePrice', {
+            required: 'Course price is required !',
+          })}
+        />
+        {errors.CoursePrice && (
+          <p style={{ color: 'red' }}>{errors.CoursePrice.message}</p>
+        )}
         <label htmlFor="txtCourseLikes">Likes : </label>
-        <input type="number" id="txtCourseLikes" className="form-control" />
+        <input
+          type="number"
+          id="txtCourseLikes"
+          className="form-control"
+          {...register('CourseLikes', {
+            required: 'Course likes is required !',
+          })}
+        />
+        {errors.CourseLikes && (
+          <p style={{ color: 'red' }}>{errors.CourseLikes.message}</p>
+        )}
         <label htmlFor="txtCourseRating">Rating : </label>
-        <input type="number" id="txtCourseRating" className="form-control" />
+        <input
+          type="number"
+          id="txtCourseRating"
+          className="form-control"
+          {...register('CourseRating', {
+            required: 'Course rating is required !',
+          })}
+        />
+        {errors.CourseRating && (
+          <p style={{ color: 'red' }}>{errors.CourseRating.message}</p>
+        )}
         <label htmlFor="txtCourseImage">Image URL : </label>
-        <input type="text" id="txtCourseImage" className="form-control" />
+        <input
+          type="text"
+          id="txtCourseImage"
+          className="form-control"
+          {...register('CourseImage', {
+            required: 'Course image is required !',
+          })}
+        />
+        {errors.CourseImage && (
+          <p style={{ color: 'red' }}>{errors.CourseImage.message}</p>
+        )}
 
         <button className="btn btn-success my-2">Add New Course</button>
       </form>
